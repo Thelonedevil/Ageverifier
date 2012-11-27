48d3c5305f9a2ab5a2ab798fac5fd338c5a13e26
Justin Wiblin <justin_wiblin@hotmail.co.uk>
2012-11-27 17:25:17 +0000

    first commit

---
+++ AgeVerifer/src/one.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ class one {
+	public static void main(String args[]) {
+		System.out.println("Hello,");
+		System.out.println("How old are you?");
+		Scanner S=new Scanner(System.in);
+		int one=S.nextInt();
+		if(one >15 )
+		{
+		System.out.println("You can enter");
+		System.out.println("PLease click the following link to enter");
+		System.out.println("www.foobar.com");
+		}
+		else
+		{
+		System.out.println("You can't enter");
+				}
+	}
+}
